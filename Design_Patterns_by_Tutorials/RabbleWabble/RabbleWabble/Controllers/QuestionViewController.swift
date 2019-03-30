7d7f79066786bab6dda1e5e2700b85e27d635bd8
Maksim Nosov <ozolc@yandex.ru>
2019-03-30 19:23:17 +0300

    Initial commit for MVC project

---
+++ Design_Patterns_by_Tutorials/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabbleWabble
+//
+//  Created by Maksim Nosov on 30/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+public class QuestionViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
