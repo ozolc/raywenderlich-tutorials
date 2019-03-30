12f56702f312cd29805148cef6030a97044810c8
Maksim Nosov <ozolc@yandex.ru>
2019-03-30 19:39:51 +0300

    Added resources and LaunchScreen

---
+++ Design_Patterns_by_Tutorials/RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Maksim Nosov on 30/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ Design_Patterns_by_Tutorials/RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Maksim Nosov on 30/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
