f1e577d14b3cf40f1f52d55dff5a967b7c33f52a
Maksim Nosov <ozolc@yandex.ru>
2019-03-31 17:01:23 +0300

    Creating a custom delegate

--- Design_Patterns_by_Tutorials/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ Design_Patterns_by_Tutorials/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -8,10 +8,28 @@
 
 import UIKit
 
+// MARK: - QuestionViewControllerDelegate
+public protocol QuestionViewControllerDelegate: class {
+    
+    // 1
+    func questionViewController(_ viewController: QuestionViewController, didCancel questionGroup: QuestionGroup, at questionIndex: Int)
+    
+    // 2
+    func questionViewController(_ viewController: QuestionViewController, didComplete questionGroup: QuestionGroup)
+}
+
 public class QuestionViewController: UIViewController {
 
     // MARK: - Instance Properties
-    public var questionGroup = QuestionGroup.basicPhrases()
+    
+    public weak var delegate: QuestionViewControllerDelegate?
+    
+    public var questionGroup: QuestionGroup! {
+        didSet {
+            navigationItem.title = questionGroup.title
+        }
+    }
+    
     public var questionIndex = 0
     
     public var correctCount = 0
@@ -22,11 +40,37 @@ public class QuestionViewController: UIViewController {
         return (view as! QuestionView)
     }
     
+    private lazy var questionIndexItem: UIBarButtonItem = {
+        let item = UIBarButtonItem(title: "",
+                                   style: .plain,
+                                   target: nil,
+                                   action: nil)
+        item.tintColor = .black
+        navigationItem.rightBarButtonItem = item
+        return item
+    }()
+    
     // MARK: - View Lifecycle
     override public func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupCancelButton()
         showQuestion()
+    }
+    
+    private func setupCancelButton() {
+        let action = #selector(handleCancelPressed(sender:))
+        let image = UIImage(named: "ic_menu")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
+                                                           landscapeImagePhone: nil,
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: action)
+    }
+    
+    @objc private func handleCancelPressed(sender: UIBarButtonItem) {
+        delegate?.questionViewController(self,
+                                         didCancel: questionGroup,
+                                         at: questionIndex)
     }
 
     private func showQuestion() {
@@ -38,6 +82,8 @@ public class QuestionViewController: UIViewController {
         
         questionView.answerLabel.isHidden = true
         questionView.hintLabel.isHidden = true
+        
+        questionIndexItem.title = "\(questionIndex + 1)/" + "\(questionGroup.questions.count)"
     }
     
     // MARK: - Actions
@@ -63,7 +109,8 @@ public class QuestionViewController: UIViewController {
     private func showNextQuestion() {
         questionIndex += 1
         guard questionIndex < questionGroup.questions.count else {
-            // TODO: - Handle this ...!
+            delegate?.questionViewController(self,
+                                             didComplete: questionGroup)
             return
         }
         showQuestion()
