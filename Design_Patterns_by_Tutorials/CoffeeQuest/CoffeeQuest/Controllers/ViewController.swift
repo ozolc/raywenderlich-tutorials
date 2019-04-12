6abc5cf0869d9f79180e45bddcc3ac41454515e7
Maksim Nosov <ozolc@yandex.ru>
2019-04-12 11:30:49 +0300

    Using Factory Pattern in CoffeeQuiz app

--- Design_Patterns_by_Tutorials/CoffeeQuest/CoffeeQuest/Controllers/ViewController.swift
+++ Design_Patterns_by_Tutorials/CoffeeQuest/CoffeeQuest/Controllers/ViewController.swift
@@ -35,6 +35,7 @@ public class ViewController: UIViewController {
   private var businesses: [YLPBusiness] = []
   private let client = YLPClient(apiKey: YelpAPIKey)
   private let locationManager = CLLocationManager()
+  public let annotationFactory = AnnotationFactory()
   
   // MARK: - Outlets
   @IBOutlet public var mapView: MKMapView! {
@@ -102,35 +103,10 @@ extension ViewController: MKMapViewDelegate {
   
   private func addAnnotations() {
     for business in businesses {
-      guard let yelpCoordinate = business.location.coordinate else {
+      guard let viewModel = annotationFactory.createBusinessMapViewModel(for: business) else {
         continue
       }
-      
-      let coordinate = CLLocationCoordinate2D(latitude: yelpCoordinate.latitude,
-                                              longitude: yelpCoordinate.longitude)
-      let name = business.name
-      let rating = business.rating
-      let image: UIImage
-      
-      // 1
-      switch rating {
-      case 0.0..<3.5:
-        image = UIImage(named: "bad")!
-      case 3.5..<4.0:
-        image = UIImage(named: "meh")!
-      case 4.0..<4.75:
-        image = UIImage(named: "good")!
-      case 4.75...5.0:
-        image = UIImage(named: "great")!
-      default:
-        image = UIImage(named: "bad")!
-      }
-      
-      let annotation = BusinessMapViewModel(coordinate: coordinate,
-                                            name: name,
-                                            rating: rating,
-                                            image: image)
-      mapView.addAnnotation(annotation)
+      mapView.addAnnotation(viewModel)
     }
   }
   
