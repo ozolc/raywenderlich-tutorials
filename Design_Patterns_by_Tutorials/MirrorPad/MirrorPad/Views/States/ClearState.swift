9ad64d6b1931b4344d3762080584233c2f473d6e
Maksim Nosov <ozolc@yandex.ru>
2019-04-15 19:25:46 +0300

    State pattern realization in MirrorPad

--- Design_Patterns_by_Tutorials/MirrorPad/MirrorPad/Views/States/ClearState.swift
+++ Design_Patterns_by_Tutorials/MirrorPad/MirrorPad/Views/States/ClearState.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2019 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,53 +28,10 @@
 
 import UIKit
 
-public class DrawView: UIView {
-  
-  // MARK: - Instance Properties
-  public var lineColor: UIColor = .black
-  public var lineWidth: CGFloat = 5.0
-  public var lines: [LineShape] = []
-  
-  public lazy var currentState = states[AcceptInputState.identifier]!
-  
-  public lazy var states = [
-    AcceptInputState.identifier: AcceptInputState(drawView: self),
-    AnimateState.identifier: AnimateState(drawView: self),
-    ClearState.identifier: ClearState(drawView: self),
-    CopyState.identifier: CopyState(drawView: self)
-  ]
-  
-  @IBInspectable public var scaleX: CGFloat = 1 {
-    didSet { applyTransform() }
-  }
-  @IBInspectable public var scaleY: CGFloat = 1 {
-    didSet { applyTransform() }
-  }
-  private func applyTransform() {
-    layer.sublayerTransform = CATransform3DMakeScale(scaleX, scaleY, 1)
-  }
-  
-  // MARK: - UIResponder
-  public override func touchesBegan(_ touches: Set<UITouch>,
-                                    with event: UIEvent?) {
-    currentState.touchesBegan(touches, with: event)
-  }
-  
-  public override func touchesMoved(_ touches: Set<UITouch>,
-                                    with event: UIEvent?) {
-    currentState.touchesMoved(touches, with: event)
-  }
-  
-  // MARK: - Actions
-  public func animate() {
-    currentState.animate()
-  }
-  
-  public func clear() {
-    currentState.clear()
-  }
-  
-  public func copyLines(from source: DrawView) {
-    currentState.copyLines(from: source)
+public class ClearState: DrawViewState {
+  public override func clear() {
+    drawView.lines = []
+    drawView.layer.sublayers?.removeAll()
+    transitionToState(matching: AcceptInputState.identifier)
   }
 }
