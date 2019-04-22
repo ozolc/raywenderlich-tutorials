536efc751a09407396abbe0833cb2dc5d03a5139
Maksim Nosov <ozolc@yandex.ru>
2019-04-22 16:10:59 +0300

    The table view delegates

--- iOS Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ iOS Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -12,9 +12,18 @@ class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        return cell
+    }
 }
 
