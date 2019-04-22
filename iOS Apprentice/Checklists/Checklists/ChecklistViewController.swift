fbc74e6b0f5032f0b3f10785c39e388e0efc0261
Maksim Nosov <ozolc@yandex.ru>
2019-04-22 16:29:40 +0300

    Putting row data into the cells

--- iOS Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ iOS Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -17,11 +17,22 @@ class ChecklistViewController: UITableViewController {
 
     // MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        switch indexPath.row {
+        case let x where x % 5 == 0: label.text = "Walk the dog"
+        case let x where x % 5 == 1: label.text = "Brush my teeth"
+        case let x where x % 5 == 2: label.text = "Learn iOS development"
+        case let x where x % 5 == 3: label.text = "Soccer practice"
+        case let x where x % 5 == 4: label.text = "Eat ice cream"
+        default: break
+        }
         
         return cell
     }
