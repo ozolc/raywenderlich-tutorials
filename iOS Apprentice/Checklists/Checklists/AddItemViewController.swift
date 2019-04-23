9db91ddc6e5736fdaf05cc4681f9942a34c339dc
Maksim Nosov <ozolc@yandex.ru>
2019-04-23 17:31:19 +0300

    Make your own view controller class

---
+++ iOS Apprentice/Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Maksim Nosov on 23/04/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+}
