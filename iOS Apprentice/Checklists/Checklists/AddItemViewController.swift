8a743d62f769403c3b32d1079d36c693ff45486e
Maksim Nosov <ozolc@yandex.ru>
2019-04-25 16:34:22 +0300

    Fixing issues

--- iOS Apprentice/Checklists/Checklists/AddItemViewController.swift
+++ iOS Apprentice/Checklists/Checklists/AddItemViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +35,22 @@ class AddItemViewController: UITableViewController {
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    // MARK: - Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    // Метод если мы удаляем через Clear button в текстовом поле (быстрая очистка)
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
