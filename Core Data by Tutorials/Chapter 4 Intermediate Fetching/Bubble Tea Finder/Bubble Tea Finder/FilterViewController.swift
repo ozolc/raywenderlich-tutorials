c92e48e06f87039269ada07024929c7fbd0ac4bd
Maksim Nosov <ozolc@yandex.ru>
2019-04-25 13:14:24 +0300

    Sorting fetched results

--- Core Data by Tutorials/Chapter 4 Intermediate Fetching/Bubble Tea Finder/Bubble Tea Finder/FilterViewController.swift
+++ Core Data by Tutorials/Chapter 4 Intermediate Fetching/Bubble Tea Finder/Bubble Tea Finder/FilterViewController.swift
@@ -91,6 +91,19 @@ class FilterViewController: UITableViewController {
     return NSPredicate(format: "%K > 0", #keyPath(Venue.stats.tipCount))
     }()
   
+  lazy var nameSortDescriptor: NSSortDescriptor = {
+    let compareSelector = #selector(NSString.localizedStandardCompare(_:))
+    return NSSortDescriptor(key: #keyPath(Venue.name), ascending: true, selector: compareSelector)
+  }()
+  
+  lazy var distanceSortDescriptor: NSSortDescriptor = {
+    return NSSortDescriptor(key: #keyPath(Venue.location.distance), ascending: true)
+  }()
+  
+  lazy var priceSortDescriptor = {
+    return NSSortDescriptor(key: #keyPath(Venue.priceInfo.priceCategory), ascending: true)
+  }()
+  
   
   // MARK: - View Life Cycle
   override func viewDidLoad() {
@@ -143,8 +156,18 @@ class FilterViewController: UITableViewController {
       selectedPredicate = walkingDistancePredicate
       case userTipsCell:
       selectedPredicate = hasUserTipsPredicate
+        
+      // Sort By section
+      case nameAZSortCell:
+        selectedSortDescriptor = nameSortDescriptor
+      case nameZASortCell:
+        selectedSortDescriptor = nameSortDescriptor.reversedSortDescriptor as? NSSortDescriptor
+      case distanceSortCell:
+        selectedSortDescriptor = distanceSortDescriptor
+      case priceSortCell:
+        selectedSortDescriptor = priceSortDescriptor
       default: break
-    }
+      }
       
       cell.accessoryType = .checkmark
     }
