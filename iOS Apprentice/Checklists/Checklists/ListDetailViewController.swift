0137a7b70ece4e57388d41dab468cdbb5534ed7f
Maksim Nosov <ozolc@yandex.ru>
2019-04-29 16:16:24 +0300

    Manage checklists

---
+++ iOS Apprentice/Checklists/Checklists/ListDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Maksim Nosov on 29/04/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController)
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController, didFinishAdding checklist: Checklist)
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController, didFinishEditing checklist: Checklist)
+}
+
+class ListDetailViewController: UITableViewController, ListDetailViewControllerDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController) {
+        <#code#>
+    }
+    
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController, didFinishAdding checklist: Checklist) {
+        <#code#>
+    }
+    
+    func listDetailViewControllerDidCancel(_ sender: ListDetailViewController, didFinishEditing checklist: Checklist) {
+        <#code#>
+    }
+    
+    
+}
