e355a6b535d69f05cbe4445d6d35f1c7bbd938dd
Maksim Nosov <ozolc@yandex.ru>
2019-04-29 15:08:07 +0300

    Chapter 16: Lists. The All Lists view controller

---
+++ iOS Apprentice/Checklists/Checklists/AllListsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Maksim Nosov on 29/04/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    
+    let cellIdentifier = "ChecklistCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier) // Регистрация идентификатора ячейки cellIdentifier, чтобы tableView знала, какую ячейку следует использовать для создания при вызове dequeue когда укажут этот идентификатор. В данном случае будет зарегистрирована стандартная ячейка UITableViewCell
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        cell.textLabel!.text = "List \(indexPath.row)"
+
+        return cell
+    }
+    
+}
