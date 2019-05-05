e0f64e39b74ff694b92f05052068c30a12e77269
Maksim Nosov <ozolc@yandex.ru>
2019-05-05 20:32:41 +0300

    Display coordinates

--- iOS Apprentice/MyLocations/MyLocations/CurrentLocationViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -12,6 +12,7 @@ import CoreLocation
 class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager() // объект, которые дает GPS координаты
+    var location: CLLocation? // Данные с координатами
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -45,7 +46,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        updateLabels()
     }
     
     // MARK:- Helper Methods
@@ -58,9 +60,24 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         
         present(alert, animated: true, completion: nil)
     }
+    
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
+    }
 
     // MARK: - CLLocationManagerDelegate
-    
     // Если ошибка получения координат
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError \(error.localizedDescription)")
@@ -70,6 +87,9 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
+        
+        location = newLocation
+        updateLabels()
     }
 
 }
