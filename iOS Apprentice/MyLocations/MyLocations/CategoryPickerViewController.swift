6f4d800ff9fb0d430c1352e436d9d4f844e387f2
Maksim Nosov <ozolc@yandex.ru>
2019-05-06 20:10:53 +0300

    The category picker. The view controller class

---
+++ iOS Apprentice/MyLocations/MyLocations/CategoryPickerViewController.swift
@@ -0,0 +1,72 @@
+//
+//  CategoryPickerViewController.swift
+//  MyLocations
+//
+//  Created by Maksim Nosov on 06/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class CategoryPickerViewController: UITableViewController {
+    var selectedCategoryName = ""
+    let cellId = "cellId"
+    
+    let categories = [
+        "No Category",
+        "Apple Store",
+        "Bar",
+        "Bookstore",
+        "Club",
+        "Grocery Store",
+        "Historic Building",
+        "House",
+        "Icecream Vendor",
+        "Landmark",
+        "Park"]
+    
+    var selectedIndexPath = IndexPath()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 0..<categories.count {
+            if categories[i] == selectedCategoryName {
+                selectedIndexPath = IndexPath(row: i, section: 0)
+                break
+            }
+        }
+    }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let categoryName = categories[indexPath.row]
+        
+        if categoryName == selectedCategoryName {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row != selectedIndexPath.row {
+            if let newCell = tableView.cellForRow(at: indexPath) {
+                newCell.accessoryType = .checkmark
+            }
+            if let oldCell = tableView.cellForRow(at: selectedIndexPath) {
+                oldCell.accessoryType = .none
+            }
+            selectedIndexPath = indexPath
+        }
+    }
+    
+}
