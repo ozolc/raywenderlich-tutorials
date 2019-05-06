83a0e084a6f7f0b8608c55d9170f03c51e9f3ac9
Maksim Nosov <ozolc@yandex.ru>
2019-05-06 18:48:53 +0300

    The new view controller

---
+++ iOS Apprentice/MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Maksim Nosov on 06/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK: - Actions
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+}
