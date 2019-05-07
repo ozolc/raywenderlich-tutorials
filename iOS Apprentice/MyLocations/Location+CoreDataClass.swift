e511f4721baed4c507ec5d6f8624b9ca31a3ac29
Maksim Nosov <ozolc@yandex.ru>
2019-05-07 15:06:40 +0300

    Add Core Data

---
+++ iOS Apprentice/MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Maksim Nosov on 07/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ iOS Apprentice/MyLocations/Location+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Maksim Nosov on 07/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date
+    @NSManaged public var locationDescription: String
+    @NSManaged public var category: String
+    @NSManaged public var placemark: CLPlacemark?
+
+}
