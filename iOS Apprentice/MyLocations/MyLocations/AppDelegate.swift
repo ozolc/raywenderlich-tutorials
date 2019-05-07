10230f762a5b2c69efea3d62c07fd6dc7d127a3a
Maksim Nosov <ozolc@yandex.ru>
2019-05-07 21:33:19 +0300

    Display Locations from data store

--- iOS Apprentice/MyLocations/MyLocations/AppDelegate.swift
+++ iOS Apprentice/MyLocations/MyLocations/AppDelegate.swift
@@ -30,10 +30,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let tabController = window!.rootViewController as! UITabBarController
         
+        // Dependency injection для managedObjectContext
+        // Первый Tab - передача managedObjectContext
         if let tabViewControllers = tabController.viewControllers {
-            let navController = tabViewControllers[0] as! UINavigationController
-            let controller = navController.viewControllers.first as! CurrentLocationViewController
-            controller.managedObjectContext = managedObjectContext
+            var navController = tabViewControllers[0] as! UINavigationController
+            let controller1 = navController.viewControllers.first as! CurrentLocationViewController
+            controller1.managedObjectContext = managedObjectContext
+        
+        // Второй Tab - передача managedObjectContext
+            navController = tabViewControllers[1] as! UINavigationController
+            let controller2 = navController.viewControllers.first as! LocationsViewController
+            controller2.managedObjectContext = managedObjectContext
         }
         
         print(applicationDocumentsDirectory)
--- iOS Apprentice/MyLocations/MyLocations/LocationsViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/LocationsViewController.swift
@@ -12,22 +12,46 @@ import CoreLocation
 
 class LocationsViewController: UITableViewController {
 
-    var managedObjectContext: NSManagedObjectContext!
+    var managedObjectContext: NSManagedObjectContext! // контекст для работы с объектами Core Data
+    var locations = [Location]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fetchRequest = NSFetchRequest<Location>() // Описание для получения из Persistent store массива объектов Location в Core Data
+        
+        let entity = Location.entity() // Инициализация Entity ассоциированную с классом Location
+        fetchRequest.entity = entity // Entity используемая fetch request
+        
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true) // Описание сортировки по полю "date" по возрастанию
+        fetchRequest.sortDescriptors = [sortDescriptor] // Массив сортировок для fetch request
+        
+        do {
+            locations = try managedObjectContext.fetch(fetchRequest)
+        } catch {
+            fatalCoreDataError(error)
+        }
+    }
     
     // MARK: - Table View Delegates
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return locations.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
         
+        let location = locations[indexPath.row] // индекс массива объектов <Locations> из Core Data
+        
         let descriptionLabel = cell.viewWithTag(100) as! UILabel
-        descriptionLabel.text = "If you can see this"
+        descriptionLabel.text = location.locationDescription
         
         let addressLabel = cell.viewWithTag(101) as! UILabel
-        addressLabel.text = "Then it works!"
-        
+        if let placemark = location.placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = ""
+        }
         return cell
     }
 
