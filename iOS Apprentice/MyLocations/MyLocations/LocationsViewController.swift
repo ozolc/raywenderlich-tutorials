70c1268a5ff2d606a6ea6281aa981fb90e55fffd
Maksim Nosov <ozolc@yandex.ru>
2019-05-08 10:55:41 +0300

    Use NSFetchedResultsController

--- iOS Apprentice/MyLocations/MyLocations/LocationsViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/LocationsViewController.swift
@@ -11,12 +11,11 @@ import CoreData
 import CoreLocation
 
 class LocationsViewController: UITableViewController {
-
+    
     var managedObjectContext: NSManagedObjectContext! // контекст для работы с объектами Core Data
     var locations = [Location]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    lazy var fetchedResultsController: NSFetchedResultsController<Location> = {
         
         let fetchRequest = NSFetchRequest<Location>() // Описание для получения из Persistent store массива объектов Location в Core Data
         
@@ -26,22 +25,46 @@ class LocationsViewController: UITableViewController {
         let sortDescriptor = NSSortDescriptor(key: "date", ascending: true) // Описание сортировки по полю "date" по возрастанию
         fetchRequest.sortDescriptors = [sortDescriptor] // Массив сортировок для fetch request
         
+        fetchRequest.fetchBatchSize = 20 // Количество получаемых записей в запросе
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                  managedObjectContext: self.managedObjectContext,
+                                                                  sectionNameKeyPath: nil,
+                                                                  cacheName: "Locations") // Сохраняем кэш на диск
+        
+        fetchedResultsController.delegate = self // При изменении в NSFetchedResultsController этот контроллер будет оповещен.
+        return fetchedResultsController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performFetch()
+        
+    }
+    
+    // MARK: - Helper methods
+    func performFetch() {
         do {
-            locations = try managedObjectContext.fetch(fetchRequest)
+            try fetchedResultsController.performFetch()
         } catch {
             fatalCoreDataError(error)
         }
     }
     
+    deinit {
+        fetchedResultsController.delegate = nil // если не используем NSFetchedResultsController, чтобы не получать оповещения.
+    }
+    
     // MARK: - Table View Delegates
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        let sectionInfo = fetchedResultsController.sections![section] // sections - возвращает массив NSFetchedResultsSectionInfo объектов, которые описывают кажду секцию table view
+        return sectionInfo.numberOfObjects
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as! LocationCell
         
-        let location = locations[indexPath.row] // индекс массива объектов <Locations> из Core Data
+        let location = fetchedResultsController.object(at: indexPath) // получаем объект из NSFetchedResultsController
         cell.configure(for: location)
         
         
@@ -55,11 +78,70 @@ class LocationsViewController: UITableViewController {
             controller.managedObjectContext = managedObjectContext
             
             if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
-                let location = locations[indexPath.row]
+                let location = fetchedResultsController.object(at: indexPath) // получаем объект из NSFetchedResultsController
                 controller.locationToEdit = location
             }
         }
     }
+    
+}
 
+// MARK:- NSFetchedResultsController Delegate Extension
+extension LocationsViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("*** controllerWillChangeContent")
+        tableView.beginUpdates() // начало изменений в table view
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            print("*** NSFetchedResultsChangeInsert (object)")
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            print("*** NSFetchedResultsChangeDelete (object)")
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            print("*** NSFetchedResultsChangeUpdate (object)")
+            if let cell = tableView.cellForRow(at: indexPath!) as? LocationCell {
+                let location = controller.object(at: indexPath!) as! Location
+                cell.configure(for: location)
+            }
+        case .move:
+            print("*** NSFetchedResultsChangeMove (object)")
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        @unknown default:
+            print("@unknown default case in didChange anObject for NSFetchedResultsController")
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            print("*** NSFetchedResultsChangeInsert (section)")
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            print("*** NSFetchedResultsChangeDelete (section)")
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .update:
+            print("*** NSFetchedResultsChangeUpdate (section)")
+        case .move:
+            print("*** NSFetchedResultsChangeMove (section)")
+        @unknown default:
+            print("@unknown default case in didChange sectionInfo for NSFetchedResultsController")
+            break
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("*** controllerDidChangeContent")
+        tableView.endUpdates() // Завершаем серию изменений в table view
+    }
+    
+    
 }
 
