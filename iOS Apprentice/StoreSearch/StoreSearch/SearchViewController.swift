69934458045fa35442b16248aa7a9b3f196e8d5a
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 19:15:56 +0300

    Section 4: Store Search. Chapter 32: Search Bar. Initial commit

---
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by Maksim Nosov on 09/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
