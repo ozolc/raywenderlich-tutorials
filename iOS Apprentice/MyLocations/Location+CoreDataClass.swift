7e18bbee4017ba25761d9140d349bb1030f46140
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 09:00:21 +0300

    Save the imageю Data model changes

--- iOS Apprentice/MyLocations/Location+CoreDataClass.swift
+++ iOS Apprentice/MyLocations/Location+CoreDataClass.swift
@@ -31,5 +31,26 @@ public class Location: NSManagedObject, MKAnnotation {
         return category
     }
     
+    var hasPhoto: Bool {
+        return photoID != nil
+    }
+    
+    var photoURL: URL {
+        assert(photoID != nil, "No photo ID set") // Утверждение, что photoID не nil, иначе вывести отладочное сообщение "No photo ID set" и завершит работу приложения.
+        let filename = "Photo-\(photoID!.intValue).jpg"
+        return applicationDocumentsDirectory.appendingPathComponent(filename)
+    }
+    
+    var photoImage: UIImage? {
+        return UIImage(contentsOfFile: photoURL.path)
+    }
+    
+    class func nextPhotoID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let currentID = userDefaults.integer(forKey: "PhotoID") + 1
+        userDefaults.set(currentID, forKey: "PhotoID")
+        userDefaults.synchronize()
+        return currentID
+    }
 
 }
