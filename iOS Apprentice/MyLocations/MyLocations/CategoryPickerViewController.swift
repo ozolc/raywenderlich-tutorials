5a4a07054a63f27c4dd64acfe523678a00e9d8c4
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 16:29:24 +0300

    Table view changes for the Category Picker screen

--- iOS Apprentice/MyLocations/MyLocations/CategoryPickerViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/CategoryPickerViewController.swift
@@ -54,6 +54,10 @@ class CategoryPickerViewController: UITableViewController {
             cell.accessoryType = .none
         }
         
+        let selection = UIView(frame: CGRect.zero)
+        selection.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
+        cell.selectedBackgroundView = selection
+        
         return cell
     }
     
