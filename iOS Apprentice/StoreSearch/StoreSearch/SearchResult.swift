f503b536913288bdba82dd84a4bffe2faf8d902b
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 21:16:26 +0300

    Create the data model

---
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Maksim Nosov on 09/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -13,7 +13,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var searchResults = [String]()
+    var searchResults = [SearchResult]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +28,11 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder() // Убрать клавиатуру
         
-        searchResults = []
         for i in 0...2 {
-            searchResults.append(String(format: "Fake Results %d for '%@'", i, searchBar.text!))
+            let searchResult = SearchResult()
+            searchResult.name = String(format: "Fake Result %d for", i)
+            searchResult.artistName = searchBar.text!
+            searchResults.append(searchResult)
         }
         tableView.reloadData()
     }
@@ -50,10 +52,12 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         
         var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
         if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         }
         
-        cell.textLabel?.text = searchResults[indexPath.row]
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel?.text = searchResult.name
+        cell.detailTextLabel?.text = searchResult.artistName
         return cell
     }
 }
