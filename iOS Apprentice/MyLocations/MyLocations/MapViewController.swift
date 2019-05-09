4f5f3f70182f1dba21d7c993605904ded29cf387
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 15:23:46 +0300

    The map screen

--- iOS Apprentice/MyLocations/MyLocations/MapViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/MapViewController.swift
@@ -135,6 +135,7 @@ extension MapViewController: MKMapViewDelegate {
             pinView.isEnabled = true
             pinView.canShowCallout = true
             pinView.animatesDrop = false
+            pinView.tintColor = UIColor(white: 0.0, alpha: 0.5)
             pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
             
             // кнопка в Annotation
