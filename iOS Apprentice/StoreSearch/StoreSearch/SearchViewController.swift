e68dadc76f0c5d53210db733b716617e10f02f58
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 20:50:35 +0300

    Add a search bar delegate

--- iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -13,14 +13,17 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0) // Добавить отступ для tableView на 64 points( 20 - StatusBar, 44 - SearchBar)
     }
 
-
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    // Этот метод срабатывает, когда пользователь нажимает Search button на клавиатуре
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
+}
