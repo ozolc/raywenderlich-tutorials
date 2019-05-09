8fc87625bcdbd0de83ee0e51187c37947bb408c5
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 21:34:50 +0300

    No results found

--- iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var searchResults = [SearchResult]()
+    var hasSearched = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +29,19 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder() // Убрать клавиатуру
         
+        searchResults = []
+        
+        if searchBar.text! != "Test" {
         for i in 0...2 {
             let searchResult = SearchResult()
             searchResult.name = String(format: "Fake Result %d for", i)
             searchResult.artistName = searchBar.text!
             searchResults.append(searchResult)
+            }
         }
+        hasSearched = true
         tableView.reloadData()
+        
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -44,7 +51,13 @@ extension SearchViewController: UISearchBarDelegate {
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -55,9 +68,29 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         }
         
-        let searchResult = searchResults[indexPath.row]
-        cell.textLabel?.text = searchResult.name
-        cell.detailTextLabel?.text = searchResult.artistName
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel?.text = searchResult.name
+            cell.detailTextLabel?.text = searchResult.artistName
+        }
         return cell
+    }
+    
+    // Снять выделение с ячейки
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        // Если нет данных в модели - выделить ячейки невозможно
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            // Разрешить выделение ячейки
+            return indexPath
+        }
     }
 }
