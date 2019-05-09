03f482ce7ec4280f9b0ed70e952b8311feb646d3
Maksim Nosov <ozolc@yandex.ru>
2019-05-09 16:34:36 +0300

    Polish the main screen

--- iOS Apprentice/MyLocations/MyLocations/CurrentLocationViewController.swift
+++ iOS Apprentice/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -36,6 +36,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
+    @IBOutlet weak var latitudeTextLabel: UILabel!
+    @IBOutlet weak var longitudeTextLabel: UILabel!
     
     // MARK: - Actions
     @IBAction func getLocation() {
@@ -124,12 +126,17 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             } else {
                 addressLabel.text = "No Address Found"
             }
+            latitudeTextLabel.isHidden = false
+            longitudeTextLabel.isHidden = false
         } else {
             
             latitudeLabel.text = ""
             longitudeLabel.text = ""
             addressLabel.text = ""
             tagButton.isHidden = true
+            
+            latitudeTextLabel.isHidden = true
+            longitudeTextLabel.isHidden = true
             
             let statusMessage: String
             if let error = lastLocationError as NSError? {
