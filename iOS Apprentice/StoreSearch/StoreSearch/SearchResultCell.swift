82d65a2ad34644a785e3349905888e277227773f
Maksim Nosov <ozolc@yandex.ru>
2019-05-10 09:01:07 +0300

    Change the look of the app

--- iOS Apprentice/StoreSearch/StoreSearch/SearchResultCell.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchResultCell.swift
@@ -14,9 +14,13 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
 
+    // awakeFromNib() вызывается после загрузки объекта ячейки в память, но перед тем как ячейка будет добавлена на tableView
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -34,6 +34,8 @@ class SearchViewController: UIViewController {
         
         cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+        
+        searchBar.becomeFirstResponder() // Отобразить клавиатуру при загрузке в строке поиска
     }
     
 }
