229f566148730c98b0ab2bb1d2f9289c66a1d319
Maksim Nosov <ozolc@yandex.ru>
2019-05-11 10:57:32 +0300

    Add the controls

--- iOS Apprentice/StoreSearch/StoreSearch/DetailViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/DetailViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
