5fa267f1969f923761ff69cfaa12a3ce236f172f
Maksim Nosov <ozolc@yandex.ru>
2019-05-12 08:38:36 +0300

    Add a scroll view

--- iOS Apprentice/StoreSearch/StoreSearch/LandscapeViewController.swift
+++ iOS Apprentice/StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Удалить constraints с main view
+        view.removeConstraints(view.constraints)
+        // Удалить constraints с page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        // Удалить constraints с scroll view
+        scrollView.removeConstraints(pageControl.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        
+        // Установка рабочей области для scroll view. Может быть больше чем bounds. Это позволяет скроллить
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Создание собственного layout. Вызывается UIKit как часть фазы расположения views на view controller'a как когда он впервые появляется на экране. Идеальное место для изменения frames для views вручную.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame // safe area для view в его собственной системе координат
+        scrollView.frame = safeFrame
+        pageControl.frame = CGRect(x: safeFrame.origin.x, y: safeFrame.size.height - pageControl.frame.size.height, width: safeFrame.size.width, height: pageControl.frame.size.height)
     }
-    */
-
 }
